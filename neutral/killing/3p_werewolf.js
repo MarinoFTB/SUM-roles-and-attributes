eb3849f1f1090df564e68e1d7bc1126d31665dd7
MarinoFTB <uniqueshaft@gmail.com>
2020-10-11 20:36:54 +0200

    werewolf win cond update

--- neutral/killing/3p_werewolf.js
+++ neutral/killing/3p_werewolf.js
@@ -40,7 +40,7 @@ module.exports.CHECK_ONLY_WHEN_GAME_ENDS = false;
 // Accepts function
 // Should key in wrt to player
 module.exports.ELIMINATED = ["mafia", "coven", "town", "3p_serial_killer", "3p_arsonist"];
-module.exports.SURVIVING = ["3p_werewolf", "3p_witch"];
+module.exports.SURVIVING = ["3p_werewolf"];
 
 module.exports.PREVENT_CHECK_ON_WIN = [];
 
